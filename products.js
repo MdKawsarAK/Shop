11d454a848de83852b2e5b9509c837b19eb76586
MdKawsarAK <kawsarhossain830@gmail.com>
2025-05-17 23:49:49 +0600

    first commit

---
+++ products.js
@@ -0,0 +1,12 @@
+let items = [
+    { "id": 10, "name": "Apple", "qty": 1, "price": 200, "photo": "apple.webp" },
+    { "id": 20, "name": "Orange", "qty": 1, "price": 250, "photo": "orange.jpg" },
+    { "id": 40, "name": "Mango", "qty": 1, "price": 250, "photo": "mango.jpg" },
+    { "id": 22, "name": "Banana", "qty": 1, "price": 250, "photo": "banana.webp" },
+    { "id": 34, "name": "Pear", "qty": 1, "price": 250, "photo": "pear.jpg" },
+    { "id": 33, "name": "Grapes", "qty": 1, "price": 150, "photo": "grapes.webp" },
+    { "id": 35, "name": "Watermelon", "qty": 1, "price": 300, "photo": "watermelon.webp" },
+    { "id": 36, "name": "Jackfruits", "qty": 1, "price": 330, "photo": "jackfruits.jpg" },
+    { "id": 37, "name": "Guava", "qty": 1, "price": 600, "photo": "guava.jpg" },
+    { "id": 39, "name": "Dates", "qty": 1, "price": 800, "photo": "dates.jpg" },
+];
